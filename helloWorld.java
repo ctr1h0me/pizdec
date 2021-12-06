ad89c3c14d7b494ce26aea0a0c1e585cae94e5d6
ctr1h0me <booleanlina@gmail.com>
2021-12-06 19:38:14 +0200

    4U

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,9 @@
 public class helloWorld {
-    public static void main (String[] args) {
-        System.out.println("you are gay");
+    public static void main(String[] args) {
+        printH("с саньком не геи");
     }
-
+    public static void printH(String h) {
+        System.out.println(h);
+    }
 }
 
