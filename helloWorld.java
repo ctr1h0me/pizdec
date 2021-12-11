2c5e33ac8066a9fc7800755917910156f9d4a6a9
ctr1h0me <booleanlina@gmail.com>
2021-12-11 18:04:34 +0200

    4U

--- helloWorld.java
+++ helloWorld.java
@@ -1,9 +1,6 @@
 public class helloWorld {
-    public static void main(String[] args) {
-        printH("с саньком не геи");
-    }
-    public static void printH(String h) {
-        System.out.println(h);
-    }
+    public static int sum(int m) { return m + 25; }
+    public static void main(String[] args) {System.out.println(sum(23));}
+
 }
 
