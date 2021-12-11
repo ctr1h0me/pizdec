534ae195922731844ba99a7b452b921b1ec99b20
ctr1h0me <booleanlina@gmail.com>
2021-12-11 18:20:25 +0200

    sos

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 public class helloWorld {
-    public static int sum(int m) { return m + 25; }
-    public static void main(String[] args) {System.out.println(sum(23));}
+    public static int convert(int m, int b) { return m * b; }
+    public static void main(String[] args) {System.out.println(convert(23, 67));}
 
 }
 
